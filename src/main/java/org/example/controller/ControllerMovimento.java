ecfdcf8285b98a267bfd05c12d5c30f7b3dcd528
Pietro <gr2353@gmail.com>
2023-09-05 14:10:33 +0200

    look

--- src/main/java/org/example/controller/ControllerMovimento.java
+++ src/main/java/org/example/controller/ControllerMovimento.java
@@ -5,10 +5,9 @@ import org.example.query.QueryMovimento;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ControllerMovimento {
 
@@ -16,32 +15,79 @@ public class ControllerMovimento {
 
         MovimentoBean movement = new MovimentoBean(123, new Date(System.currentTimeMillis()), "prelievo");
         QueryMovimento queryMovimento = new QueryMovimento();
+        ResultSet resultSet;
+        PrintWriter writer;
+        Scanner sc = new Scanner(System.in);
+        int response;
+
 
         try{
             //queryMovimento.insertMovimento(movement , 5, con);
 
+            System.out.print("Quale operazione di vuole effettuare ? " +
+                    "\n 1- Inserimento " +
+                    "\n 2- Select Singola dei movimento " +
+                    "\n 3- Select Bulk " +
+                    "\n Inserire la risposta:");
 
-            ResultSet resultSet = queryMovimento.selectMovimentsFromClient(5,con);
-            PrintWriter writer = new PrintWriter(System.getProperty("user.home") + "\\" +"Desktop" + "\\prova.txt",
-                    "UTF-8");
+            response = sc.nextInt();
 
 
-            while (resultSet.next()) {
-                writer.println();
-                writer.print("Movimento{"+
-                        "tipo movimento: "+resultSet.getString(1)+" "+
-                        "data movimento: "+resultSet.getDate(2)+" "+
-                        "totale movimento: "+resultSet.getInt(3)+" "+
-                        "utente del movimento: "+resultSet.getString(4)+" "+
-                        "}");
+            switch(response){
+                case 1:
+                    queryMovimento.insertMovimento(movement , 4,con);
+                    break;
+
+                case 2:
+                    resultSet = queryMovimento.selectMovimento(con);
+                    writer = new PrintWriter(System.getProperty("user.home") + "\\" +"Desktop" + "\\prova.txt",
+                            "UTF-8");
+
+
+                    while (resultSet.next()) {
+                        writer.println();
+                        writer.print("Movimento{"+
+                                "tipo movimento: "+resultSet.getString(1)+" "+
+                                "data movimento: "+resultSet.getDate(2)+" "+
+                                "totale movimento: "+resultSet.getInt(3)+" "+
+                                "utente del movimento: "+resultSet.getString(4)+" "+
+                                "}");
+                    }
+
+                    writer.close();
+                    break;
+
+                case 3:
+                    resultSet = queryMovimento.selectMovimentsFromClient(5,con);
+                    writer = new PrintWriter(System.getProperty("user.home") + "\\" +"Desktop" + "\\prova.txt",
+                            "UTF-8");
+
+
+                    while (resultSet.next()) {
+                        writer.println();
+                        writer.print("Movimento{"+
+                                "tipo movimento: "+resultSet.getString(1)+" "+
+                                "data movimento: "+resultSet.getDate(2)+" "+
+                                "totale movimento: "+resultSet.getInt(3)+" "+
+                                "utente del movimento: "+resultSet.getString(4)+" "+
+                                "}");
+                    }
+
+                    writer.close();
+                    break;
+
             }
 
-            writer.close();
 
 
-        }catch(UnsupportedEncodingException | NullPointerException | FileNotFoundException | SQLException e){
+        }catch(UnsupportedEncodingException | InputMismatchException | NullPointerException | FileNotFoundException | SQLException e){
             System.out.print(e.getMessage());
         }
+
+
+
+
+
 
     }
 }
